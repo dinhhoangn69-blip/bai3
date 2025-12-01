dd9320fafca29df735dc8e61a99efde7f2abbd74
dinhhoangn69-blip <dinhhoangn69@gmail.com>
2025-12-01 08:32:56 +0700

    bai3_4

---
+++ bài3_4.py
@@ -0,0 +1,12 @@
+print("Sinh vien: Nguyen Dinh Hoang")
+print("Ma so SV : 245752021610043")
+print("###########################")
+#################################
+a = "Hello Guy!"
+
+def say(a):
+  a = "Vinh University"
+  print(a)
+
+say(a)
+print(a)
