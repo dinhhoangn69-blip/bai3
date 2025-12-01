6dd0465ba1fe1cb0e943e136f9b71a0d69d66585
dinhhoangn69-blip <dinhhoangn69@gmail.com>
2025-12-01 08:33:46 +0700

    bai3_7

---
+++ bài3_7.py
@@ -0,0 +1,11 @@
+print("Sinh vien: Nguyen Dinh Hoang")
+print("Ma so SV : 245752021610043")
+print("###########################")
+#################################
+def checkValue(n):
+  if n % 2 == 0:
+    print("Day la mot so chan")
+  else:
+    print("Day la mot so le")
+
+checkValue(6)
