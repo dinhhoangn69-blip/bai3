82052b45a9dc542fb92386effa4893e872521b93
dinhhoangn69-blip <dinhhoangn69@gmail.com>
2025-12-01 08:31:42 +0700

    bai3_1

---
+++ bài3_1.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Nguyen Dinh Hoang")
+print("Ma so SV : 245752021610043")
+print("###########################")
+#################################
+def sum(a, b):
+  print("sum = " + str(a + b))
+
+sum(4, 5)
+sum(3, 7)
